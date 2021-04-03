c926a24a2e33b104e8ea8e4e8810ddeefb9708c0
Horn Sliver <52667243@qq.com>
2021-04-03 11:37:29 +0800

    Add minimal home page

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask
-from flask import escape
+from flask import escape,url_for
 
 app = Flask(__name__)
 
-@app.route('/home')
+@app.route('/')
 def hello():
 	return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
 
 @app.route('/user/<name>')
 def user_page(name):
-	return 'User: %s' % escape(name)
+	return 'User: %s ' % escape(name)
+
+@app.route('/test')
+def test_url_for():
+	print(url_for('hello'))
+	print(url_for('user_page', name='Horn'))
+	print(url_for('user_page', name='Silver'))
+	print(url_for('test_url_for'))
+	print(url_for('test_url_for',num1=2))
+	return 'Test page'
